322f21cd15f8af6e44a1d56816a16b8e56188784
acelerier002 <adrien.celerier@etu.u-bordeaux.fr>
2019-01-22 19:03:54 +0100

    zelda-like

---
+++ ZeldaLike-RPG/ZeldaLike-Unity/Assets/FollowGameObject.cs
@@ -0,0 +1,21 @@
+﻿/* Author : Raphaël Marczak - 2018, for Label[i]
+ * 
+ * This work is licensed under the CC0 License. 
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGameObject : MonoBehaviour {
+    public GameObject m_objectToFollow;
+	
+	// At each frame, the camera position is set to the "objetToFollow" x and y coordinates
+    // (z remains the one of the camera, to respect the depth)
+	void Update () {
+        this.transform.position = new Vector3(m_objectToFollow.transform.position.x,
+                                              m_objectToFollow.transform.position.y,
+                                              this.transform.position.z);
+	}
+}
---
+++ ZeldaLike-RPG/ZeldaLike-Unity/Assets/Teleport.cs
@@ -0,0 +1,50 @@
+﻿/* Author : Raphaël Marczak - 2018, for Label[i]
+ * 
+ * This work is licensed under the CC0 License. 
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class is used to teleport the player from one scene to another
+// If the scene is different, then the previous scene is disabled and the new
+// one is enabled ; and the map indication is updated accordingly
+public class Teleport : MonoBehaviour {
+    public GameObject m_teleportTo = null;
+  
+    private GameObject m_player = null;
+
+    private void Awake()
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+     
+        if (collision.gameObject.tag == "Player")
+        {
+            TeleportPlayer();
+        }
+
+    }
+
+    private void TeleportPlayer()
+    {
+        if (m_teleportTo != null)
+        {
+            if (this.transform.parent != null)
+            {
+                this.transform.parent.gameObject.SetActive(false);
+                m_teleportTo.transform.parent.gameObject.SetActive(true);
+
+                m_player.transform.position = m_teleportTo.transform.position;
+            }
+        }
+    }
+
+    
+
+}
